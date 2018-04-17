5a339b49192380136795cd5779d6fd89306cbc5c
Thomas Mitchell <tmitchell@starkandwayne.com>
2018-04-17 14:20:32 -0400

    Add userpass auth with sessions

---
+++ server/auth.go
@@ -0,0 +1,93 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/pborman/uuid"
+)
+
+var sessions = map[string]time.Time{}
+
+func newSession() string {
+	u := uuid.NewUUID()
+	sessions[u.String()] = time.Now().Add(30 * time.Minute)
+	return u.String()
+}
+
+func validateSession(sessionID string) bool {
+	if expiry, found := sessions[sessionID]; found {
+		if time.Now().Before(expiry) {
+			return true
+		}
+
+		delete(sessions, sessionID)
+	}
+
+	return false
+}
+
+type userpassAuth struct {
+	Username string `json:"username" yaml:"username"`
+	Password string `json:"password" yaml:"password"`
+}
+
+func newUserpassAuth(conf map[string]string) (*userpassAuth, error) {
+	if conf == nil {
+		return nil, fmt.Errorf("No auth config provided")
+	}
+
+	if conf["username"] == "" {
+		return nil, fmt.Errorf("No username provided in userpass auth config")
+	}
+
+	if conf["password"] == "" {
+		return nil, fmt.Errorf("No password provided in userpass auth config")
+	}
+
+	return &userpassAuth{
+		Username: conf["username"],
+		Password: conf["password"],
+	}, nil
+}
+
+func (u userpassAuth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	provided := userpassAuth{}
+	err = json.Unmarshal(body, &provided)
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	if provided.Username != u.Username || provided.Password != u.Password {
+		w.WriteHeader(401)
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf(`{"token":"%s"}`+"\n", newSession())))
+}
+
+type authorizer func(http.HandlerFunc) http.HandlerFunc
+
+func nopAuth(fn http.HandlerFunc) http.HandlerFunc { return fn }
+
+func sessionAuth(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sessionID := r.Header.Get("X-Doomsday-Token")
+		if validateSession(sessionID) {
+			fn(w, r)
+		} else {
+			w.WriteHeader(401)
+		}
+	}
+}
--- server/server.go
+++ server/server.go
@@ -15,6 +15,10 @@ import (
 type Config struct {
 	Port    int    `yaml:"port"`
 	LogFile string `yaml:"logfile"`
+	Auth    struct {
+		Type   string            `yaml:"type"`
+		Config map[string]string `yaml:"config"`
+	} `yaml:"auth"`
 }
 
 type server struct {
@@ -30,6 +34,8 @@ func Start(conf Config, core *doomsday.Core) error {
 			return fmt.Errorf("Could not open log file for writing: %s", err)
 		}
 	}
+
+	fmt.Fprintf(logWriter, "Initializing server\n")
 
 	populate := func() {
 		err := core.Populate()
@@ -47,10 +53,36 @@ func Start(conf Config, core *doomsday.Core) error {
 		}
 	}()
 
-	router := mux.NewRouter()
-	router.HandleFunc("/v1/cache", getCache(core)).Methods("GET")
-	router.HandleFunc("/v1/cache/refresh", refreshCache(core)).Methods("POST")
+	fmt.Fprintf(logWriter, "Began asynchronous cache population\n")
 
+	var shouldNotAuth bool
+	var authHandler http.Handler
+	switch conf.Auth.Type {
+	case "":
+		shouldNotAuth = true
+	case "userpass":
+		authHandler, err = newUserpassAuth(conf.Auth.Config)
+	default:
+		err = fmt.Errorf("Unrecognized auth type `%s'", conf.Auth.Type)
+	}
+	if err != nil {
+		return fmt.Errorf("Error setting up auth: %s", err)
+	}
+
+	fmt.Fprintf(logWriter, "Auth configured with type `%s'\n", conf.Auth.Type)
+
+	var auth authorizer
+	router := mux.NewRouter()
+	if shouldNotAuth {
+		auth = nopAuth
+	} else {
+		router.Handle("/v1/auth", authHandler).Methods("POST")
+		auth = sessionAuth
+	}
+	router.HandleFunc("/v1/cache", auth(getCache(core))).Methods("GET")
+	router.HandleFunc("/v1/cache/refresh", auth(refreshCache(core))).Methods("POST")
+
+	fmt.Fprintf(logWriter, "Beginning listening on port %d\n", conf.Port)
 	return http.ListenAndServe(fmt.Sprintf(":%d", conf.Port), router)
 }
 
