2c3acf2ffb6e0f1964aeeaa3fdf62c2c34514110
Thomas Mitchell <klaue@live.com>
2018-05-23 22:39:31 -0400

    Support cert chains

--- core.go
+++ core.go
@@ -115,15 +115,52 @@ func (b *Core) Paths() (storage.PathList, error) {
 }
 
 func parseCert(c string) *x509.Certificate {
-	pemBlock, _ := pem.Decode([]byte(c))
-	if pemBlock == nil {
+	certChain := []*x509.Certificate{}
+	//Populate a potential chain of certs (or even just one) into this here slice
+	for pemBlock, rest := pem.Decode([]byte(c)); len(rest) > 0; pemBlock, rest = pem.Decode(rest) {
+		//Skip over potential private keys in a cert chain.
+		if pemBlock == nil || pemBlock.Type != "CERTIFICATE" {
+			continue
+		}
+
+		cert, err := x509.ParseCertificate(pemBlock.Bytes)
+		if err != nil {
+			continue
+		}
+
+		certChain = append(certChain, cert)
+	}
+
+	return getLeafCert(certChain)
+}
+
+//I'm assuming that given a cert chain, the chain is either from server to
+//highest intermediate/root CA, or in the reverse order. You know - not just
+//some random smattering of certs, because that doesn't work with anything I
+//know of. Therefore, I check the first two certs in the chain
+//for signature direction to try to determine which is the server cert, and
+//then return only that. If the thing that signed it is in the chain and we
+//can actually control it, its probably somewhere else in the storage and
+//it'll be caught at another key.
+func getLeafCert(chain []*x509.Certificate) *x509.Certificate {
+	if len(chain) == 0 {
 		return nil
 	}
 
-	cert, err := x509.ParseCertificate(pemBlock.Bytes)
-	if err != nil {
-		return nil
+	//This applies if the chain is len 1, or if the chain is leaf to root
+	ret := chain[0]
+
+	if len(chain) > 1 {
+		//Check if the first cert signed the second cert
+		rootToLeaf := chain[0].CheckSignature(
+			chain[1].SignatureAlgorithm,
+			chain[1].RawTBSCertificate,
+			chain[1].Signature) != nil
+
+		if rootToLeaf {
+			ret = chain[len(chain)-1]
+		}
 	}
 
-	return cert
+	return ret
 }
