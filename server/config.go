c0d48c1515960c6f152ffb8370560de554dd6b4b
Thomas Mitchell <klaue@live.com>
2018-06-06 16:15:03 -0400

    set default server port (8111)

--- server/config.go
+++ server/config.go
@@ -12,18 +12,20 @@ import (
 
 type Config struct {
 	Backend storage.Config `yaml:"backend"`
-	Server  struct {
-		Port    uint16 `yaml:"port"`
-		LogFile string `yaml:"logfile"`
-		TLS     struct {
-			Cert string `yaml:"cert"`
-			Key  string `yaml:"key"`
-		} `yaml:"tls"`
-		Auth struct {
-			Type   string            `yaml:"type"`
-			Config map[string]string `yaml:"config"`
-		} `yaml:"auth"`
-	} `yaml:"server"`
+	Server  APIConfig      `yaml:"server"`
+}
+
+type APIConfig struct {
+	Port    uint16 `yaml:"port"`
+	LogFile string `yaml:"logfile"`
+	TLS     struct {
+		Cert string `yaml:"cert"`
+		Key  string `yaml:"key"`
+	} `yaml:"tls"`
+	Auth struct {
+		Type   string            `yaml:"type"`
+		Config map[string]string `yaml:"config"`
+	} `yaml:"auth"`
 }
 
 func ParseConfig(path string) (*Config, error) {
@@ -37,12 +39,31 @@ func ParseConfig(path string) (*Config, error) {
 		return nil, fmt.Errorf("Could not read from config (%s): %s", path, err)
 	}
 
-	conf := Config{}
-	envPort, err := strconv.ParseUint(os.Getenv("PORT"), 10, 16)
-	conf.Server.Port = uint16(envPort)
+	//Set defaults
+	conf := Config{
+		Server: APIConfig{
+			Port: 8111,
+		},
+	}
+
+	if os.Getenv("PORT") != "" {
+		envPort, err := strconv.ParseUint(os.Getenv("PORT"), 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("Could not convert port to int")
+		}
+
+		conf.Server.Port = uint16(envPort)
+	}
+
+	//Read config
 	err = yaml.Unmarshal(fileContents, &conf)
 	if err != nil {
 		return nil, fmt.Errorf("Could not parse config (%s) as YAML: %s", path, err)
+	}
+
+	//Validation
+	if conf.Server.Port < 0 || conf.Server.Port > 65535 {
+		return nil, fmt.Errorf("Port number is invalid")
 	}
 
 	return &conf, nil
