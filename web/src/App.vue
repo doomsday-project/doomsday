62a5a56b84e10785292e8ca675ced817b0870086
Thomas Mitchell <klaue@live.com>
2021-02-11 00:33:20 -0500

    port login page design. logic pending

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <Navigation/>
-    <div id="nav-buffer"/>
-    <router-view/>
+    <div id="viewport">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import Vue from 'vue'
+import Component from 'vue-class-component';
 import Navigation from './components/navigation/Navigation.vue';
 
 @Component({
@@ -19,19 +21,38 @@ export default class App extends Vue {}
 </script>
 
 <style>
+html {
+  margin: 0;
+}
+
 body {
   margin: 0;
 }
 
 #app {
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  color: white;
 }
 
-#nav-buffer {
+#viewport {
+  display: flex;
+  align-items:     center;
+  justify-content: center;
   margin-top: 60px;
+  flex: 1;
+  overflow-y: auto;
+	background: repeating-linear-gradient(
+		45deg,
+		rgba(68, 68, 68, 1),
+		rgba(68, 68, 68, 1) 60px,
+		rgba(68, 68, 68, 0.85) 60px,
+		rgba(68, 68, 68, 0.85) 120px
+  );
 }
 </style>
--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,9 +1,33 @@
 <template>
-  <div id="viewport">
-    <h1>LOGIN</h1>
-    <button v-on:click="$router.push({name: 'Dashboard'})">
-      to dashboard
-    </button>
+  <div id="login-box">
+    <form id="login-form">
+
+      <div class="login-row">
+        <label for="login-form-username-input">username</label>
+        <input type="text"
+               name="username"
+               id="login-form-username-input"
+               placeholder="username"
+               autofocus
+        />
+      </div>
+
+      <div class="login-row">
+        <label for="login-form-password-input">password</label>
+        <input type="password"
+               name="password"
+               id="login-form-password-input"
+               placeholder="************"
+        />
+      </div>
+
+      <div class="login-button-row">
+        <input type="submit" id="login-form-submit-button" value="log in"/>
+      </div>
+
+    </form>
+
+    <div v-if="errorMessage" class="login-error">{{ errorMessage }}</div>
   </div>
 </template>
 
@@ -11,5 +35,86 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  errorMessage = "";
+}
 </script>
+
+<style scoped>
+#login-box {
+  background-color: #252525;
+  border-radius: 14px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  width: 325px;
+  padding: 1em;
+}
+
+#login-form {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+}
+
+.login-row {
+  width: 300px;
+  margin: 0.75em 1em;
+  text-align: left;
+}
+
+.login-row label {
+  padding-right: 0.5em;
+  margin-left: 4px;
+  margin-bottom: 4px;
+  display: grid;
+  font-size: 16px;
+}
+
+.login-row input[type=text], input[type=password] {
+  height: 25px;
+  font-size: 20px;
+  border-radius: 6px;
+  font-family: inherit;
+  padding: 0.25em 10px;
+  width: 275px;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  outline: none;
+  box-shadow: 0 0 0 2px rgb(229, 53, 69);
+}
+
+.login-button-row {
+  height: 30px;
+  width: 300px;
+  margin: 0.75em 0em;
+}
+.login-button-row input[type=submit] {
+  height: 30px;
+  width: 300px;
+  font-size: 20px;
+  font-family: inherit;
+  border-style: outset;
+  border-width: 2px;
+  border-radius: 6px;
+  border-color: white;
+  background: rgba(37,37,37,1);
+  color: white;
+}
+
+.login-button-row input[type=submit]:hover {
+  background: rgba(255,255,255,0.1) !important;
+}
+
+.login-button-row input[type=submit]:active {
+  background: rgba(255,255,255,0.2) !important;
+}
+
+.login-button-row input[type=submit]:focus {
+  outline: none;
+}
+
+</style>
